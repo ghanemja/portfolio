415c8b1f471f30d170244cd845fba0ffe9cb0f70
ghanemja <janghanem@gmail.com>
2022-12-04 00:07:37 -0500

    grammar error

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
       <video 
         ref={videoRef} 
         onCanPlay={() => setPlayBack()} 
